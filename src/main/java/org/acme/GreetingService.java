08d8d6a556b19267946311a243a6716912465cac
rflsousa <rafael.oliveira.osv@gmail.com>
2023-05-24 20:56:45 -0300

    first commit

---
+++ src/main/java/org/acme/GreetingService.java
@@ -0,0 +1,12 @@
+package org.acme;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+
+}
